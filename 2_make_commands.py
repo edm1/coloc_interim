39c70fc011428adfb1cfbad0a854d09e0b0de6f4
Edward Mountjoy <emountjoy@gmail.com>
2018-09-08 18:30:02 +0100

    make commands finished

--- 2_make_commands.py
+++ 2_make_commands.py
@@ -11,10 +11,20 @@ def main():
     # Args
     global args
     args = {}
+    # Input args
     args['in_mani'] = 'configs/coloc_manifest_180908.csv'
     args['in_prot_map'] = 'configs/Sun_pQTL_SOMALOGIC_GWAS_protein_info.ensembl.manifest.tsv'
     args['in_tissue_map'] = 'configs/tissue_codes.tsv'
+    # File patterns
+    args['coloc_wrapper'] = 'scripts/coloc_wrapper.py'
+    args['bgen'] = 'input_data/uk10k/bgen/{chrom}.ALSPAC_TWINSUK.maf01.beagle.anno_updated.csq.shapeit.20131101.bgen'
+    args['disease'] = 'input_data/gwas/genome_wide/{study_id}/{trait_id}/{chrom}-{study_id}-{trait_id}.tsv.gz'
+    args['eqtl'] = 'input_data/molecular_qtl/eqtl/GTEX7/{tissue}/{biomarker}/{chrom}-GTEX7-{tissue}-{biomarker}.tsv.gz'
+    args['pqtl'] = 'input_data/molecular_qtl/pqtl/SUN2018/{tissue}/{biomarker}/{chrom}-SUN2018-{tissue}-{biomarker}.tsv.gz'
+    args['out_pref'] = 'output/coloc_180908/{study_id}/{study_id}-{variant_id}-{source_id}-{tissue}-{biomarker}'
+    # Coloc args
     args['coloc_window_kb'] = 500
+    # Other args
     args['tmp_dir'] = 'tmp'
 
     # Make temp dir
@@ -23,11 +33,80 @@ def main():
     # Load manifest
     mani = load_manifest()
     mani = mani.loc[mani.chrom == '22', :].head(1) # DEBUG
-    mani.to_csv('tmp/manifest_merged.tsv.gz', sep='\t', index=None, compression='gzip')
+    mani.to_csv(args['tmp_dir'] + '/manifest_merged.tsv.gz', sep='\t',
+                index=None, compression='gzip')
 
-
+    # Make commands
+    commands = mani.apply(make_commands, axis=1)
+    commands.to_csv(args['tmp_dir'] + '/commands.txt', sep='\t', index=None)
+    # for command in commands:
+    #     print(command)
 
     return 0
+
+def make_commands(row):
+    ''' Makes command to run coloc using wrapper
+    Args:
+        row (pd.Series)
+    Returns:
+        str containing shell command
+    '''
+    # Make input path names
+    in_bgen = os.path.abspath(args['bgen'].format(
+        chrom=row.chrom))
+    in_disease_ss = os.path.abspath(args['disease'].format(
+        study_id=row.stid,
+        trait_id=row.stid.replace('NEALEUKB', 'UKB'),
+        chrom=row.chrom))
+
+    if row.source_id == 'gtex_v7':
+        in_mol_ss = os.path.abspath(args['eqtl'].format(
+            tissue=row.uberon_code,
+            biomarker=row.gene_id,
+            chrom=row.chrom))
+        outf = os.path.abspath(args['out_pref'].format(
+            study_id=row.stid,
+            tissue=row.uberon_code,
+            biomarker=row.gene_id,
+            variant_id=row.index_variant_id,
+            source_id=row.source_id,
+            chrom=row.chrom))
+
+    elif row.source_id == 'sun2018':
+        in_mol_ss = os.path.abspath(args['pqtl'].format(
+            tissue=row.uberon_code,
+            biomarker=row.UniProt,
+            chrom=row.chrom))
+        outf = os.path.abspath(args['out_pref'].format(
+            study_id=row.stid,
+            tissue=row.uberon_code,
+            biomarker=row.UniProt,
+            variant_id=row.index_variant_id,
+            source_id=row.source_id,
+            chrom=row.chrom))
+
+    else:
+        sys.exit('Error: source_id {0} not recognised'.format(row.source_id))
+
+    # Temp dir
+    out_tmp = os.path.abspath(
+        args['tmp_dir'] + '/coloc_wrapper/{study_id}'.format(
+            study_id=row.stid))
+
+    # Make command
+    cmd = [
+        'python',
+        os.path.abspath(args['coloc_wrapper']),
+        '--left_sumstats', in_disease_ss,
+        '--right_sumstats', in_mol_ss,
+        '--bgen', in_bgen,
+        '--outpref', outf,
+        '--pos', row.pos,
+        '--window_kb', args['coloc_window_kb'],
+        '--tmpdir', out_tmp
+    ]
+
+    return ' '.join([str(x) for x in cmd])
 
 def load_manifest():
     ''' Loads the manifest and merges required columns
