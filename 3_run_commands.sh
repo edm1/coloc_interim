09f713d73c79541dfb9d85786ed6521b160b9ba6
edm1 <emountjoy@gmail.com>
2018-09-11 09:11:13 +0100

    log which commands are done

--- 3_run_commands.sh
+++ 3_run_commands.sh
@@ -11,13 +11,20 @@ in_commands=tmp/commands.txt.gz
 
 echo START...
 
+# Remove log files
+rm -f tmp/commands.processing.log.txt.gz
+rm -f tmp/commands.already_complete.log.txt.gz
+
 zcat < $in_commands | while read cmd
 do
     # Parse outfiles
     outfile=$(echo $cmd | cut -f 10 -d " ")/COMPLETE
     # If COMPLETE file does not exist, run command
     if [ ! -f $outfile ]; then
+        echo $cmd | gzip -c >> tmp/commands.processing.log.txt.gz
         echo $cmd
+    else
+        echo $cmd | gzip -c >> tmp/commands.already_complete.log.txt.gz
     fi
 done | parallel -j $cores
 
